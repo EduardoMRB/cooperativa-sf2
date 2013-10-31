782b653224a5bfa1e790d952e08519fe4bfdd92c
Eduardo Borges <eduardomrb@gmail.com>
2013-10-31 19:35:02 -0300

    Adiciona classe admin inicial para Relatorio de Entidades

---
+++ src/Cooperativa/Bundle/AdminBundle/Admin/RelatorioEntidadeAdmin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Cooperativa\Bundle\AdminBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+
+class RelatorioEntidadeAdmin extends Admin
+{
+    
+}
